7788e61620cfd9bbba5a981a5b04306928779e7e
ARCassiano <anderson.cassiano@hotmail.com>
2018-08-24 10:08:05 -0300

    Desafio LINQ

---
+++ DesafioProcessoSeletivo/Classes/BancoTesteContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Configuration.Assemblies;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioProcessoSeletivo.Classes
+{
+    /* Classe responsável por gerenciar a conexão com o banco de dados e todas as suas opeções */
+    class BancoTesteContext : DbContext
+    {
+        /* O DbSet Usuario fica responsável por mapear a tabela Usuario o banco de dados */
+        public DbSet<Usuario> Usuario { get; set; }
+
+        /* Constrtutor Default */
+        public BancoTesteContext()
+        {
+        }
+
+        /* Método herdado de DbContext e com sobrecarga na classe filha, responsável por realizar as configurações iniciais  */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            /* String de conexão com o Banco de dados */
+            optionsBuilder.UseSqlServer(
+                "Data Source=Ntbk_vaio;Initial Catalog=BancoTeste;User ID=UsuarioAplicao;Password=MinhaSenhaSeguraDaAplicacao");
+        }
+    }
+}
---
+++ DesafioProcessoSeletivo/Classes/Usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace DesafioProcessoSeletivo.Classes
+{
+    /* Classe Model para Usuário */
+    public class Usuario
+    {
+        /* Método construtor da classe Usuario */
+        public Usuario(string nome, string email, bool ativo)
+        {
+            this.Nome = nome;
+            this.Email = email;
+            this.Ativo = ativo;
+        }
+
+        /* Atributos, getters e setters da classe Usuario */
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
+
